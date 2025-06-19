e5def005acffd2618257cbf80c85bd78f5625290
Umer Adnan <umeradnan1705@gmail.com>
2025-06-19 16:28:19 +0500

    first commit

---
+++ app/(dashboard)/page.tsx
@@ -0,0 +1,17 @@
+"use client"; 
+import { useNewAccount } from "@/features/accounts/hooks/use-new-account";
+import { Button } from "@/components/ui/button";
+
+export default function Home() {
+  const { onOpen } = useNewAccount(); 
+
+  return (
+    <div>
+      <Button onClick={onOpen}>
+        Add an account
+      </Button>
+    </div>
+  ); 
+};
+
+
