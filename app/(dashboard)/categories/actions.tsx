c0f98788789f547244beff9457f30f859c07b239
Umer Adnan <umeradnan1705@gmail.com>
2025-06-22 12:42:40 +0500

    actions, columns and page set up for categories in dashboard

--- app/(dashboard)/categories/actions.tsx
+++ app/(dashboard)/categories/actions.tsx
@@ -9,9 +9,10 @@ import {
 } from "@/components/ui/dropdown-menu"
 import { Delete, Edit, MoreHorizontal, Trash } from "lucide-react";
 
-import { useOpenAccount } from "@/features/accounts/hooks/use-open-account";
-import { useDeleteAccount } from "@/features/accounts/api/use-delete-account";
+import { useOpenCategory } from "@/features/categories/hooks/use-open-category";
+import { useDeleteCategory } from "@/features/categories/api/use-delete-category";
 import { useConfirm } from "@/hooks/use-confirm";
+
 
 type Props = {
     id: string; 
@@ -20,11 +21,11 @@ type Props = {
 export const Actions = ( { id }: Props) => {
     const [ConfirmDialog, confirm] = useConfirm(
         "Are you sure?", 
-        "You are about to delete this transaction"
+        "You are about to delete this category"
     ); 
 
-    const deleteMutation = useDeleteAccount(id); 
-    const { onOpen } = useOpenAccount(); 
+    const deleteMutation = useDeleteCategory(id); 
+    const { onOpen } = useOpenCategory(); 
     const handleDelete = async () => {
         const ok = await confirm(); 
         if (ok) {
--- app/(dashboard)/categories/page.tsx
+++ app/(dashboard)/categories/page.tsx
@@ -10,24 +10,24 @@ import {
     CardHeader,
     CardTitle,
 } from "@/components/ui/card"; 
-import { useNewAccount } from "@/features/accounts/hooks/use-new-account";
-import { useGetAccounts } from "@/features/accounts/api/use-get-accounts";
-import { useBulkDeleteAccounts } from "@/features/accounts/api/use-bulk-delete";
+import { useNewCategory } from "@/features/categories/hooks/use-new-category";
+import { useBulkDeleteCategories } from "@/features/categories/api/use-bulk-delete-categories";
+import { useGetCategories } from "@/features/categories/api/use-get-categories";
 
 import { columns } from "./columns";
 import { Skeleton } from "@/components/ui/skeleton";
 
 const CategoriesPage = () => {
-    const newAccount = useNewAccount(); 
-    const deleteAccounts = useBulkDeleteAccounts(); 
-    const accountsQuery = useGetAccounts(); 
-    const accounts = accountsQuery.data || []; 
+    const NewCategory = useNewCategory(); 
+    const deleteCategories = useBulkDeleteCategories(); 
+    const categoriesQuery = useGetCategories(); 
+    const categories = categoriesQuery.data || []; 
 
     const isDisabled = 
-        accountsQuery.isLoading || 
-        deleteAccounts.isPending; 
+        categoriesQuery.isLoading || 
+        deleteCategories.isPending; 
 
-    if (accountsQuery.isLoading) {
+    if (categoriesQuery.isLoading) {
         return (
             <div className="max-w-screen-2xl mx-auto w-full pb-10 -mt-24">
                 <Card className="border-none drop-shadow-sm">
@@ -51,7 +51,7 @@ const CategoriesPage = () => {
                     <CardTitle className="text-xl line-clamp-1">
                         Categories page
                     </CardTitle>
-                    <Button onClick={newAccount.onOpen} size="sm">
+                    <Button onClick={NewCategory.onOpen} size="sm">
                         <Plus className="size-4 mr-2"/>
                         Add new
                     </Button>
@@ -60,10 +60,10 @@ const CategoriesPage = () => {
                     <DataTable 
                         filterKey="name"
                         columns={columns} 
-                        data={accounts} 
+                        data={categories} 
                         onDelete={(row) => {
                             const ids = row.map((r) => r.original.id); 
-                            deleteAccounts.mutate({ ids }); 
+                            deleteCategories.mutate({ ids }); 
                         }}
                         disabled={isDisabled}
                     />
