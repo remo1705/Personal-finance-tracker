4e3cb042cf7667bee989cae362372867e809166e
Umer Adnan <umeradnan1705@gmail.com>
2025-06-24 13:09:55 +0500

    created account and category values to be passed into the form

--- features/transactions/components/new-transaction-sheet.tsx
+++ features/transactions/components/new-transaction-sheet.tsx
@@ -8,7 +8,13 @@ import {
 } from "@/components/ui/sheet"; 
 import { insertTransactionSchema } from "@/db/schema";
 import { z } from "zod"; 
-import { useCreateTransaction } from "@/features/transactions/api/use-create-transaction"; 
+
+import { useGetCategories } from "@/features/categories/api/use-get-categories";
+import { useCreateCategory } from "@/features/categories/api/use-create-category";
+
+import { useCreateTransaction } from "../api/use-create-transaction";
+import { useGetAccounts } from "@/features/accounts/api/use-get-accounts";
+import { useCreateAccount } from "@/features/accounts/api/use-create-account";
 
 const formSchema = insertTransactionSchema.omit({
     id: true, 
@@ -20,6 +26,27 @@ export const NewTransactionSheet = () => {
     const { isOpen, onClose } = useNewTransaction(); 
 
     const mutation = useCreateTransaction(); 
+
+    const categoryQuery = useGetCategories(); 
+    const categoryMutation = useCreateCategory(); 
+    const onCreateCategory = (name: string) => categoryMutation.mutate({
+        name
+    }); 
+    const categoryOptions = (categoryQuery.data ?? []).map((category) => ({
+        label: category.name, 
+        value: category.id, 
+    })); 
+
+    const accountQuery = useGetAccounts(); 
+    const accountMutation = useCreateAccount(); 
+    const onCreateAccount = (name: string) => accountMutation.mutate({
+        name
+    }); 
+    const accountOptions = (accountQuery.data ?? []).map((account) => ({
+        label: account.name, 
+        value: account.id, 
+    })); 
+
 
     const onSubmit = (values: FormValues) => {
         mutation.mutate(values, {
