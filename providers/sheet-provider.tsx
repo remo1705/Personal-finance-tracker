f9deb45f9248b733291d8fdfc4b307eb97d91b50
Umer Adnan <umeradnan1705@gmail.com>
2025-06-24 12:58:54 +0500

    developing transactions page

--- providers/sheet-provider.tsx
+++ providers/sheet-provider.tsx
@@ -6,6 +6,9 @@ import { useMountedState } from "react-use";
 import { NewCategorySheet } from "@/features/categories/components/new-category-sheet"; 
 import { EditCategorySheet } from "@/features/categories/components/edit-category-sheet";
 
+import { NewTransactionSheet } from "@/features/transactions/components/new-transaction-sheet";
+
+
 export const SheetProvider = () => {
     const isMounted = useMountedState(); 
     if (!isMounted) return null; 
@@ -17,6 +20,8 @@ export const SheetProvider = () => {
 
             <NewCategorySheet />
             <EditCategorySheet />
+
+            <NewTransactionSheet />
         </>
     )
 }
