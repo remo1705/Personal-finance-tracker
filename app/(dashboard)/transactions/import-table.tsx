dce3d53a1060f3aeeb6b48cb8cda2ae415ee5a43
Umer Adnan <umeradnan1705@gmail.com>
2025-06-27 21:40:27 +0500

    Headers drop down is working - options in the headers' dropdown to work (function to develop em remaining)

--- app/(dashboard)/transactions/import-table.tsx
+++ app/(dashboard)/transactions/import-table.tsx
@@ -6,6 +6,7 @@ import {
     TableHeader,
     TableRow, 
  } from "@/components/ui/table"; 
+import { TableHeadSelect } from "./table-head-select";
 
  type Props = {
     headers: string[]; 
@@ -27,7 +28,11 @@ import {
                     <TableRow>
                         {headers.map((_item, index) => (
                             <TableHead key={index}>
-                                {index}
+                                <TableHeadSelect
+                                    columnIndex={index}
+                                    selectedColumns={selectedColumns}
+                                    onChange={onTableHeadSelectChange} 
+                                />
                             </TableHead>
                         ))}
                     </TableRow>
