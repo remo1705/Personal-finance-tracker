7250869fd377349517ddda0f847cdaf11b445bae
Umer Adnan <umeradnan1705@gmail.com>
2025-06-27 19:08:16 +0500

    implemented the filter by payee in transactions page

--- app/(dashboard)/transactions/page.tsx
+++ app/(dashboard)/transactions/page.tsx
@@ -58,7 +58,7 @@ const TransactionsPage = () => {
                 </CardHeader>
                 <CardContent>
                     <DataTable 
-                        filterKey="name"
+                        filterKey="payee"
                         columns={columns} 
                         data={transactions} 
                         onDelete={(row) => {
