5c404fcca5cf5aaf088e146d4ab47cc3cfd2e90c
Umer Adnan <umeradnan1705@gmail.com>
2025-06-30 20:09:53 +0500

    development of data card to contain data grid

--- components/data-grid.tsx
+++ components/data-grid.tsx
@@ -1,9 +1,13 @@
 "use client"; 
 
+import { useGetSummary } from "@/features/summary/api/use-get-summary";
 import { formatDateRange } from "@/lib/utils";
 import { useSearchParams } from "next/navigation";
+import { FaPiggyBank } from "react-icons/fa";
+import { DataCard } from "@/components/data-card";
 
 export const DataGrid = () => {
+    const {data} = useGetSummary(); 
     const params = useSearchParams(); 
     const to = params.get("to") || undefined; 
     const from = params.get("from") || undefined; 
@@ -11,7 +15,15 @@ export const DataGrid = () => {
     const dateRangeLabel = formatDateRange({ to, from }); 
     return (
         <div className="grid grid-cols-1 lg:grid-cols-3 gap-8 pb-2 mb-8">
-            {dateRangeLabel}
+            <DataCard 
+                title="Remaining"
+                value={data?.remainingAmount}
+                percentageChange={data?.remainingChange}
+                icon={FaPiggyBank}
+                variant="default"
+                dateRange={dateRangeLabel}
+            />
+
         </div>
     ); 
 }; 
