7bb9c91af86ae625d4a451ec8cec735af77bbe0c
Umer Adnan <umeradnan1705@gmail.com>
2025-07-01 13:56:47 +0500

    styling of data cards displaying income, expenses and remaining amounts in the summary page

--- components/data-card.tsx
+++ components/data-card.tsx
@@ -1,6 +1,9 @@
 import { IconType } from "react-icons"; 
 import { VariantProps, cva } from "class-variance-authority";
-import { cn } from "@/lib/utils"; 
+import { cn, formatCurrency, formatPercentage } from "@/lib/utils"; 
+import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "@/components/ui/card";
+import { CountUp } from "@/components/count-up"; 
+import { Skeleton } from "./ui/skeleton";
 
 const boxVariant = cva(
     "rounded-md p-3", 
@@ -46,11 +49,68 @@ interface DataCardProps extends BoxVariants, IconVariants {
     dateRange: string; 
     percentageChange?: number; 
 }
-
-export const DataCard = ({}: DataCardProps) => {
+// styling data card
+export const DataCard = ({
+    icon: Icon, 
+    title, 
+    value=0, 
+    variant, 
+    dateRange, 
+    percentageChange=0, 
+}: DataCardProps) => {
     return (
-        <div>
-            Data Card 
-        </div>
-    )
-}
+        <Card className="border-none drop-shadow-sm">
+            <CardHeader className="flex flex-row items-center justify-between gap-x-4">
+                <div className="space-y-2">
+                    <CardTitle className="text-2xl line-clamp-1">
+                        {title} 
+                    </CardTitle>
+                    <CardDescription className="line-clamp-1">
+                        {dateRange}
+                    </CardDescription>
+                </div>
+                <div className={cn(boxVariant({ variant }), 
+                )}>
+                    <Icon className={cn(iconVariant({ variant }))}/>
+                </div>
+            </CardHeader> 
+            <CardContent>
+                <h1 className="font-bold text-2xl mb-2 line-clamp-1 break-all">
+                    <CountUp 
+                        preserveValue
+                        start={0}
+                        end={value}
+                        decimals={2}
+                        decimalPlaces={2}
+                        formattingFn={formatCurrency}
+                    />
+                </h1>
+                <p className={cn(
+                    "text-muted-foreground text-sm line-clamp-1", 
+                    percentageChange > 0 && "text-emerald-500", 
+                    percentageChange < 0 && "text-rose-500", 
+                )}>
+                    {formatPercentage(percentageChange)} from last period
+                </p>
+            </CardContent>
+        </Card>
+    ); 
+}; 
+
+export const DataCardLoading = () => {
+    return (
+        <Card className="border-none drop-shadow-sm h-[192px]">
+            <CardHeader className="flex flex-row items-center justify-between gap-x-4">
+                <div className="space-y-2">
+                    <Skeleton className="h-6 w-24"/>
+                    <Skeleton className="h-4 w-40"/>
+                </div>
+                <Skeleton className="size-12"/>
+            </CardHeader>
+            <CardContent>
+                <Skeleton className="shrink-0 h-10 w-24 mb-2"/>
+                <Skeleton className="shrink-0 h-4 w-40"/>
+            </CardContent>
+        </Card>
+    ); 
+}; 
